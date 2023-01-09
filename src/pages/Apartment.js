40bb2438d169e0d8e581adfd28cf5b0d4e7b4603
Jérémy Verrier <jeremy.verrier2a@gmail.com>
2023-01-09 12:29:03 +0100

    Modification page Apartment.js pour être redirigé vers la page Error404.js si l'ID de l'appartement n'est pas correcte

--- src/pages/Apartment.js
+++ src/pages/Apartment.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
 
-
-
 // import des components Collaps, Tags, Rate et Carousel
 import Carousel from '../Components/Carousel';
 import Tags from '../Components/Tags';
@@ -13,42 +11,47 @@ import Collaps from '../Components/Collaps';
 import Apartments from '../Data/Apartment.json'
 
 
-
-
 const Apartment = () => {
     //Recuperation de l'id de l'apartement dans l'URL & des information concernant celui-ci
     const {id} = useParams();
     const Apartment = Apartments.find(apartment => apartment.id === id);
-    const {title, pictures, host, location, rating, equipments, description, tags } = Apartment;
-    
-return (
-    
-    <div className='Apartment__main'>
-        <div className='Apartment__cover'>
-            <Carousel pictures={pictures}/>
-        </div>
-        <div className='Apartment__container'>
-            <div className='Apartment__title__profile'>
-                <h1 className='Apartment__title'>{title}</h1> 
-                <p className='Apartment__location'>{location}</p> 
-                {tags.map((tag, index) => 
-                <Tags key={index} getTags={tag}/>)}  
-            </div>
+   
+    if (Apartment === undefined) {
+        window.location.replace("http://localhost:3000/error404");
+    }
+    else{
+     const {title, pictures, host, location, rating, equipments, description, tags } = Apartment;    
+    return (
         
-            <div className='Apartment__rating'>
-                <div className='user__profile'>
-                    <span className='user'>{host.name}</span>
-                    <img className='profile' src={host.picture} alt={host.name}/>
+        <div className='Apartment__main'>
+            <div className='Apartment__cover'>
+                <Carousel pictures={pictures}/>
+            </div>
+            <div className='Apartment__container'>
+                <div className='Apartment__title__profile'>
+                    <h1 className='Apartment__title'>{title}</h1> 
+                    <p className='Apartment__location'>{location}</p> 
+                    {tags.map((tag, index) => 
+                    <Tags key={index} getTags={tag}/>)}  
                 </div>
-                <Rate number={rating} />
+            
+                <div className='Apartment__rating'>
+                    <div className='user__profile'>
+                        <span className='user'>{host.name}</span>
+                        <img className='profile' src={host.picture} alt={host.name}/>
+                    </div>
+                    <Rate number={rating} />
+                </div>
+            </div>
+            <div className='Apartment__collaps'>
+                <Collaps title="Description" content={description} />
+                <Collaps title="Équipements" content={equipments} />
             </div>
         </div>
-        <div className='Apartment__collaps'>
-            <Collaps title="Description" content={description} />
-            <Collaps title="Équipements" content={equipments} />
-        </div>
-    </div>
-
-)}
+    
+    )}
+    
+    
+}
 
 export default Apartment;
